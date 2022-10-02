df789b2d8ec81d595d5517df63c59dc99063cc4a
Konrad Koni <eventbeate@gmail.com>
2022-10-02 10:05:49 +0200

    wtf am I doing

--- tests/api/disposable_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule Psglx.Commands.DisposableTest do
-  use ExUnit.Case, async: true
-
-  alias Psglx.Commands.Disposable
-
-  test "disposable/1 returns true for disposable emails" do
-    assert Disposable.disposable("koni@yopmail.com")
--- tests/psglx_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PsglxTest do
-  use ExUnit.Case
-  doctest Psglx
-
-  test "greets the world" do
-    assert Psglx.hello() == :world
-  end
-end
